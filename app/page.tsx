272bea5fc39cd722729ddde02f6efbc07d8ab296
William Chua <william.eduard123@gmail.com>
2026-07-30 11:46:04 +0800

    fix: force serverside render getting private env from server

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
